71177479bbd937a3c5cf18d874b58b9715ba616e
luannt <luannt2@sendo.vn>
2024-01-01 02:34:07 +0700

    chg: update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,12 +4,12 @@ const Footer = () => {
   return (
     <footer className="container mx-auto flex h-24 w-full items-center justify-center border-t px-10">
       <a
-        className="flex items-center justify-center gap-2"
+        className="flex items-center justify-center gap-2 text-white"
         href="https://github.com/ThinhMDITPTIT"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Powered by <strong>Luan Nguyen (Lucian)</strong>
+        Powered by <strong >Lucian (Luận Nguyễn)</strong>
       </a>
     </footer>
   );
